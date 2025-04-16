d82f3738270237a543651a6cac6d4c10fdf70a18
rahulprops <rahulhack912@gmail.com>
2025-04-16 15:17:11 +0530

    userSchema crete

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import 'dotenv/config.js'
 import dbConntect from './utils/db.js';
-
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 const app = express()
 const port=process.env.PORT || 3000;
 
+//! middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cookieParser())
+
+const corsOptions={
+    origin:"",
+    credentials:true
+}
+app.use(cors(corsOptions))
 
 
 //! server start
---
+++ backend/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const UserSchema=new Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    phoneNumber:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["Student","Recruiter"],
+        default:"Student",
+    },
+    profile:{
+         bio:{
+           type:String, 
+         },
+         skills:[{
+            type:String
+         }],
+         resume:{
+            type:String,   // url to resume file
+         },
+         rusumeOriginalName:{type:String},
+         company:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Company"
+        },
+        profilePhoto:{
+            type:String,
+            default:""
+        }
+    },
+
+},{timestamps:true})
+const User=model("User",UserSchema)
+export default User;
