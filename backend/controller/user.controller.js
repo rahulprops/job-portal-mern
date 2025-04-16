d9415e4020b96874afd7f6a855e35690b59e3f57
rahulprops <rahulhack912@gmail.com>
2025-04-16 16:36:11 +0530

    logout create

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -66,3 +66,12 @@ import bcrypt from 'bcrypt'
          return res.status(500).json({message:`server error ${error.message}`})
     }
  }
+
+ //! logout user
+  export const logout=async (req,res)=>{
+    try {
+         return res.status(200).cookie("token","",{maxAge:0}).json({message:"user logout sucesful"})
+    } catch (error) {
+        return res.status(500).json({message:`server error ${error.message}`})
+    }
+  }
